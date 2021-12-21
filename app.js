8da6a9c0a80d8bd3902c40621d66089cad77e8eb
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2021-12-21 13:45:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Selecting Menu button and the hidden menu - adding click event to menu button
+const menuButton = document.getElementById('menuButton').addEventListener('click', openNav);
+const hiddenMenu = document.getElementById('flexHiddenMenu');
+const menuClose = document.getElementById('closeOut').addEventListener('click', closeNav);
+const fullContainer = document.getElementById('fullContainer');
+
+
+// New from w3schools example
+
+function openNav() {
+    hiddenMenu.style.width = "275px";
+    /* In addition to changing the width to make the side menu appear
+    I need to change the styling of the background to have a tint to it
+    think I can add a i
+    */
+    fullContainer.setAttribute('class', 'tint');
+    // fullContainer.style.zIndex = "-1";
+}
+
+function closeNav() {
+    hiddenMenu.style.width = "0";
+    fullContainer.removeAttribute('class');
+}
+
