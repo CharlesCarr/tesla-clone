c00cdccc9cbc866bcccd7cad3425cedb69516d6e
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-01-07 16:55:29 -0500

    improving format/comments

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-// TESTING FOR GIT/GITHUB - 2nd Commit
+// Variables for grabbing elements for events
+const menuButton = document.getElementById("menuButton");
+const hiddenMenu = document.getElementById("flexHiddenMenu");
+const menuClose = document.getElementById("closeOut");
+const fullContainer = document.getElementById("fullContainer");
 
+// Adding Click Event Listeners
+menuButton.addEventListener("click", openNav);
+menuClose.addEventListener("click", closeNav);
 
-
-// Selecting Menu button and the hidden menu - adding click event to menu button
-const menuButton = document.getElementById('menuButton').addEventListener('click', openNav);
-const hiddenMenu = document.getElementById('flexHiddenMenu');
-const menuClose = document.getElementById('closeOut').addEventListener('click', closeNav);
-const fullContainer = document.getElementById('fullContainer');
-
-
-// New from w3schools example
-
+// Function to Open Side Navigation
 function openNav() {
-    hiddenMenu.style.width = "275px";
-    /* In addition to changing the width to make the side menu appear
-    I need to change the styling of the background to have a tint to it
-    think I can add a i
-    */
-    fullContainer.setAttribute('class', 'tint');
-    // fullContainer.style.zIndex = "-1";
+  hiddenMenu.style.width = "275px";
+  fullContainer.setAttribute("class", "tint");
 }
 
+// Function to Close Side Navigation
 function closeNav() {
-    hiddenMenu.style.width = "0";
-    fullContainer.removeAttribute('class');
+  hiddenMenu.style.width = "0";
+  fullContainer.removeAttribute("class");
 }
-
